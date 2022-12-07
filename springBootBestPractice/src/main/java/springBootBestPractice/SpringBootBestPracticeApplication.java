3458be7d33ea854e184cca1b551af563abbbc27a
dyparkkk <dkdlel1z@naver.com>
2022-12-07 16:18:48 +0900

    init

---
+++ springBootBestPractice/src/main/java/springBootBestPractice/SpringBootBestPracticeApplication.java
@@ -0,0 +1,13 @@
+package springBootBestPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBestPracticeApplication.class, args);
+	}
+
+}
---
+++ springBootBestPractice/src/test/java/springBootBestPractice/SpringBootBestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package springBootBestPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
