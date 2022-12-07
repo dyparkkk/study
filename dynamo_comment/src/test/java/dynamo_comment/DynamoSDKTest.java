52a7bc44382696509a4f3bfd8ae40966b1ef4830
dyparkkk <dkdlel1z@naver.com>
2022-12-07 09:31:53 +0900

    아마존 sdk로 테이블 생성, 아이템 삽입 테스트 코드 작성

--- dynamo_comment/src/test/java/dynamo_comment/DynamoSDKTest.java
+++ dynamo_comment/src/test/java/dynamo_comment/DynamoSDKTest.java
@@ -1,20 +1,78 @@
 package dynamo_comment;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbWaiter;
+
+import java.net.URI;
+import java.util.HashMap;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 public class DynamoSDKTest {
     private DynamoDbClient ddc;
+    private String key;
 
+    @BeforeEach
     public void init() {
-        ProfileCredentialsProvider profileCredentialsProvider = ProfileCredentialsProvider.create();
-
         ddc = DynamoDbClient.builder()
-                .credentialsProvider(profileCredentialsProvider)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .endpointOverride(URI.create("http://localhost:8000"))
+                .region(Region.AP_NORTHEAST_2)
                 .build();
+
+        key = "key";
     }
 
-    void create
+    @Test
+    void 테이블_생성(){
+        DynamoDbWaiter dbWaiter = ddc.waiter();
+        CreateTableRequest req = CreateTableRequest.builder()
+                .attributeDefinitions(AttributeDefinition.builder()
+                        .attributeName(key)
+                        .attributeType(ScalarAttributeType.S)
+                        .build())
+                .keySchema(KeySchemaElement.builder()
+                        .attributeName(key)
+                        .keyType(KeyType.HASH)
+                        .build())
+                .provisionedThroughput(ProvisionedThroughput.builder()
+                        .readCapacityUnits(1L)
+                        .writeCapacityUnits(1L)
+                        .build())
+                .tableName("myTable")
+                .build();
+
+        CreateTableResponse createTableResponse = ddc.createTable(req);
+
+        then(createTableResponse.tableDescription().tableName()).isEqualTo("myTable");
+    }
+
+    @Test
+    void 아이템_삽입(){
+        HashMap<String, AttributeValue> itemValues = new HashMap<>();
+
+        itemValues.put(key, AttributeValue.builder().s("3").build());
+        itemValues.put("name", AttributeValue.builder().s("doyoung").build());
+        itemValues.put("hello", AttributeValue.builder().s("world").build());
+
+        PutItemRequest req = PutItemRequest.builder()
+                .tableName("myTable")
+                .item(itemValues)
+                .build();
+
+        try {
+            PutItemResponse putItemResponse = ddc.putItem(req);
+            System.out.println("req is " + putItemResponse.responseMetadata().requestId());
+
+        } catch (ResourceNotFoundException e) {
+            System.err.println(" 존재하지 않음 ");
+        }
+    }
+
 
 }
