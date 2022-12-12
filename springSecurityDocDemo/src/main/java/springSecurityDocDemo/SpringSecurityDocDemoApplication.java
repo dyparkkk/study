fe4c5cec11b353662405d5654e902eefffae9c65
dyparkkk <dkdlel1z@naver.com>
2022-12-12 18:03:28 +0900

    init

---
+++ springSecurityDocDemo/src/main/java/springSecurityDocDemo/SpringSecurityDocDemoApplication.java
@@ -0,0 +1,13 @@
+package springSecurityDocDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDocDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDocDemoApplication.class, args);
+	}
+
+}
---
+++ springSecurityDocDemo/src/test/java/springSecurityDocDemo/SpringSecurityDocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springSecurityDocDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDocDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
