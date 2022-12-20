c0ecdd1975a483c8358fd3d1a6bc2f5d245ecaa9
dyparkkk <dkdlel1z@naver.com>
2022-12-20 17:10:04 +0900

    feat: jdbcUserDetailsManager at config

--- springSecurityDocDemo/src/main/java/springSecurityDocDemo/config/WebSecurityConfig.java
+++ springSecurityDocDemo/src/main/java/springSecurityDocDemo/config/WebSecurityConfig.java
@@ -2,20 +2,26 @@ package springSecurityDocDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,20 +34,22 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder, DataSource dataSource){
         UserDetails user = User.builder()
                 .username("username")
                 .password(passwordEncoder.encode("password"))
                 .roles("USER")
                 .build();
-
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("password")
+                .password(passwordEncoder().encode("password"))
                 .roles("ADMIN", "USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager; // UserDetailsService를 상속받음
     }
 
     @Bean
@@ -49,4 +57,11 @@ public class WebSecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
 }
